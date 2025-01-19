66b39a904418f0378d7d52a7cec8b09d876a7649
aibek <aibek_shermatov@macbookpro.home>
2025-01-19 09:12:47 +0100

    Updated project files

--- React/project-management-react/src/pages/ProjectList/ProjectList.jsx
+++ React/project-management-react/src/pages/ProjectList/ProjectList.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { MixerHorizontalIcon } from '@radix-ui/react-icons';
+import React from 'react';
 
 const ProjectList = () => {
   return (
-    
     <>
-      <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
-        <section className='filter section'>
-          <Card>
-            
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5">
+        {/* Filter Section */}
+        <section className="filterSection">
+          <Card className="p-5 sticky top-10">
+            <div className="flex justify-between lg:w-[20rem]">
+              <p className="text-xl tracking-wider">filter</p>
+              <Button variant="ghost" size="icon">
+                <MixerHorizontalIcon />
+              </Button>
+            </div>
+            <CardContent className="mt-5">
+              <ScrollArea className="space-y-7 h-[70vh]">
+              <div>
+                <h1 className='pb-3 text-gray-400 border-b'>
+                  Category
+                </h1>
+                <div>
+                  
+                </div>
+              </div>
+              </ScrollArea>
+            </CardContent>
           </Card>
-
         </section>
 
-        <section className='projectListSection'>
-
+        {/* Project List Section */}
+        <section className="projectListSection w-full lg:w-[48rem]">
+          
+          {/* Add project list items here */}
         </section>
-
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProjectList
+export default ProjectList;
